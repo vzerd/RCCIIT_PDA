78468a08981f1053077485502e3d8d73d4b15c07
“Tiasha <tiasha1234hait@gmail.com>
2024-08-15 17:23:35 +0530

    Initial Page: Added Basic Nav.

--- Frontend/pda_frontend/src/App.jsx
+++ Frontend/pda_frontend/src/App.jsx
@@ -4,9 +4,14 @@ function App() {
 
 
   return (
-    <>
-
-    </>
+   <>
+   <div className="w-full h-24 bg-[#8BD5D3] flex justify-between items-center p-3">
+      <h1 className="text-3xl font-bold text-[#475d66] pl-4">RCCIIT <span className="font-normal text-3xl">Placement Data Analysis</span></h1>
+      <div className="flex items-center space-x-2 pr-4">
+  
+      </div>
+    </div>
+   </>
   )
 }
 
