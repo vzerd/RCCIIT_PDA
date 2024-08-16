dc518d5709ca5d228c3c9900597df1721a356139
Vivek Dey <dey.vivek.official@gmail.com>
2024-08-16 22:10:45 +0530

    Init commit. Created Auth API v1, backed with MySQL.

---
+++ Backend/pda_backend/src/main/java/com/rcciitpda/pda_backend/Controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.rcciitpda.pda_backend.Controller;
+
+import com.rcciitpda.pda_backend.Model.Users;
+import com.rcciitpda.pda_backend.Service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/v1/auth")
+public class AuthController {
+
+    AuthService authService;
+    @Autowired
+    AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/sign_in")
+    ResponseEntity<String> signIn(@RequestBody Users user){
+        return authService.signInService(user);
+    }
+
+    @PostMapping("/sign_out")
+    ResponseEntity<String> signOut(@RequestBody Users user){
+        return authService.signOutService(user);
+    }
+}
