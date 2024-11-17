bfef5057cb2d40421f0d98f4727b68ff77c2214e
Vivek Dey <dey.vivek.official@gmail.com>
2024-11-17 11:51:53 +0530

    Fixed CORS Error on endpoints.

--- Backend/pda_backend/src/main/java/com/rcciitpda/pda_backend/Controller/AuthController.java
+++ Backend/pda_backend/src/main/java/com/rcciitpda/pda_backend/Controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://rcciit-pda-webpage.s3-website.ap-south-1.amazonaws.com")
 @RequestMapping("/api/v1/auth")
 public class AuthController{
 
